990c70f735e68a86ea06791302ca5864d021b1c1
Lewis Benson <tm.lewis.benson@gmail.com>
2022-11-28 15:19:47 -0500

    add test to server, make server module

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 'use strict';
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
+// require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const PORT = process.env.PORT || 3002;
 
 
+// app.use(cors());
+
+
+// app.get('/', (req, res, next)=>{
+//   res.status(200).send('Hello World');
+// });
+
+// app.listen(PORT, ()=> console.log(`listening on port: ${PORT}`));
+
+const server = require('./server');
+
+server.start();
 
